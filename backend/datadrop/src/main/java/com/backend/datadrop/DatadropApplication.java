6319aedf97a245562f4fe0af9abb41086296b76e
kamstand <kamrul.hassan@stonybrook.edu>
2025-07-19 22:40:00 -0400

    created db and new schema

---
+++ backend/datadrop/src/main/java/com/backend/datadrop/DatadropApplication.java
@@ -0,0 +1,13 @@
+package com.backend.datadrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatadropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatadropApplication.class, args);
+    }
+
+}
---
+++ backend/datadrop/src/test/java/com/backend/datadrop/DatadropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.datadrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatadropApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
